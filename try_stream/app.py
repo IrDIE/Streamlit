fd26008ae376d37c3407f160f0c35a25c374c762
IrDIE <iradynina00@mail.ru>
2023-08-03 16:36:27 +0300

    try out host

--- try_stream/app.py
+++ try_stream/app.py
@@ -1,23 +1,27 @@
 import streamlit as st
+from streamlit_webrtc import webrtc_streamer
 import cv2
 
 
-cap = cv2.VideoCapture(0)
+
 
 st.title('Video capturing from OpenCV')
 
 frame_ph = st.empty()
 stop_stream_bottom = st.button('Stop')
-
-while cap.isOpened() and not stop_stream_bottom:
-    success, img = cap.read()
-
-    frame_ph.image(img, channels="BGR")
-    cv2.imshow("Show from client webcam", img)
-    if cv2.waitKey(1) & 0xFF == ord('q') or stop_stream_bottom:
-        break
-
-
-
+# cap = cv2.VideoCapture(0)
+# while cap.isOpened() and not stop_stream_bottom:
+#     success, img = cap.read()
+#
+#     frame_ph.image(img, channels="BGR")
+#     cv2.imshow("Show from client webcam", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q') or stop_stream_bottom:
+#         break
 # cap.release()
 # cv2.destroyAllWindows()
+
+webrtc_streamer(key="example",
+                rtc_configuration={  # Add this line
+                    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+                }
+                )
