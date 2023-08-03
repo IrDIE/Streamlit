04f08b3f61492eeaddb8c04550fae877abbaa598
IrDIE <iradynina00@mail.ru>
2023-08-03 20:41:24 +0300

    Update app.py

--- try_stream/app.py
+++ try_stream/app.py
@@ -21,47 +21,35 @@ stop_stream_bottom = st.button('Stop')
 # cv2.destroyAllWindows()
 
 webrtc_streamer(key="example"
-                , rtc_configuration=
+                , rtc_configuration=  # stun.l.google.com:19305
                 {
                     "iceServers": [
                         {
-                            "urls": "stun:stun.relay.metered.ca:80",
-                        },
-                        {
-                            "urls": "turn:a.relay.metered.ca:80",
-                            "username": "0e053deab7300457f9ecbb03",
-                            "credential": "QlkpT5au/NwMGyNj",
-                        },
-                        {
-                            "urls": "turn:a.relay.metered.ca:80?transport=tcp",
-                            "username": "0e053deab7300457f9ecbb03",
-                            "credential": "QlkpT5au/NwMGyNj",
-                        },
-                        {
-                            "urls": "turn:a.relay.metered.ca:443",
-                            "username": "0e053deab7300457f9ecbb03",
-                            "credential": "QlkpT5au/NwMGyNj",
-                        },
-                        {
-                            "urls": "turn:a.relay.metered.ca:443?transport=tcp",
-                            "username": "0e053deab7300457f9ecbb03",
-                            "credential": "QlkpT5au/NwMGyNj",
-                        },
+                            "urls": ["stun:stun.l.google.com:19305"],
+                        }
+
+                        # ,
+                        # {
+                        #     "urls": "turn:a.relay.metered.ca:80",
+                        #     "username": "0e053deab7300457f9ecbb03",
+                        #     "credential": "QlkpT5au/NwMGyNj",
+                        # },
+                        # {
+                        #     "urls": "turn:a.relay.metered.ca:80?transport=tcp",
+                        #     "username": "0e053deab7300457f9ecbb03",
+                        #     "credential": "QlkpT5au/NwMGyNj",
+                        # },
+                        # {
+                        #     "urls": "turn:a.relay.metered.ca:443",
+                        #     "username": "0e053deab7300457f9ecbb03",
+                        #     "credential": "QlkpT5au/NwMGyNj",
+                        # },
+                        # {
+                        #     "urls": "turn:a.relay.metered.ca:443?transport=tcp",
+                        #     "username": "0e053deab7300457f9ecbb03",
+                        #     "credential": "QlkpT5au/NwMGyNj",
+                        # },
                     ],
                 }
                 )
 
-
-
-###########
-# RTCConfiguration(
-# {"iceServers":[{"urls": ["stun:eu-turn6.xirsys.com"]},
-# {"username": user,
-# "credential": cred,
-# "urls": ["turn:eu-turn6.xirsys.com:80?transport=udp",
-# "turn:eu-turn6.xirsys.com:3478?transport=udp",
-# "turn:eu-turn6.xirsys.com:80?transport=tcp",
-# "turn:eu-turn6.xirsys.com:3478?transport=tcp",
-# "turns:eu-turn6.xirsys.com:443?transport=tcp",
-# "turns:eu-turn6.xirsys.com:5349?transport=tcp"]}]}
-# )
