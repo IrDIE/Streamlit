6d0eacfcf22a4f68c0e1b423dc3001126604d76f
IrDIE <iradynina00@mail.ru>
2023-08-04 19:56:49 +0300

    /

--- try_stream/app.py
+++ try_stream/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+#from streamlit_webrtc import webrtc_streamer
 import cv2
+import mediapipe as mp
 import os
 import av
 import time
