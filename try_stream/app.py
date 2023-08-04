cd44cd52130ed90a215771d32dfa228e854c3e86
IrDIE <iradynina00@mail.ru>
2023-08-04 17:20:00 +0300

    /

--- try_stream/app.py
+++ try_stream/app.py
@@ -1,39 +1,6 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import cv2
-import av
-import time
-import mediapipe as mp
-import time
-import math
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import hand_tracking_module
-
-
-
-st.title('Video capturing from OpenCV')
-
-frame_ph = st.empty()
-stop_stream_bottom = st.button('Stop')
-
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = interface.QueryInterface(IAudioEndpointVolume)
-volume.GetMasterVolumeLevel()
-min, max = volume.GetVolumeRange()[0], volume.GetVolumeRange()[1]
-st.markdown(f"min max = {min}, {max}")
-volume.SetMasterVolumeLevelScalar(0/100, None)
-
-def video_frame_callback(frame: av.VideoFrame, volume = volume) -> av.VideoFrame:
-    detector = hand_tracking_module.handDetector(min_tracking_confidence=.9)
-
-    image = frame.to_ndarray(format="bgr24")
-    img = detector.findHands(image)
-
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-
 
 webrtc_streamer(key="example"
                 , rtc_configuration=
@@ -56,6 +23,6 @@ webrtc_streamer(key="example"
                     ]
                 },
 media_stream_constraints={"video": True, "audio": False},
-                video_frame_callback=video_frame_callback
+
                 )
 
