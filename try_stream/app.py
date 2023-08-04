6d238ad267bdc7a266165d03b35a19b73883a73d
IrDIE <iradynina00@mail.ru>
2023-08-04 20:09:20 +0300

    Update app.py

--- try_stream/app.py
+++ try_stream/app.py
@@ -14,22 +14,23 @@ import math
 webrtc_streamer(key="example"
                 , rtc_configuration=
                 {
-                    "iceServers": [
-                        {
-                            "urls": ["stun:fr-turn2.xirsys.com"]
-                        }, {
-                            "username": "PhUZzBq2wugeeSwzRxiwKCluHmp2OrZVfKvSWUMKVKJNK8B08EiCqVacNkLcfxkvAAAAAGTMeP1JcmluYQ==",
-                            "credential": "1f5dbe1c-327c-11ee-a998-0242ac120004",
-                            "urls": [
-                                "turn:fr-turn2.xirsys.com:80?transport=udp",
-                                "turn:fr-turn2.xirsys.com:3478?transport=udp",
-                                "turn:fr-turn2.xirsys.com:80?transport=tcp",
-                                "turn:fr-turn2.xirsys.com:3478?transport=tcp",
-                                "turns:fr-turn2.xirsys.com:443?transport=tcp",
-                                "turns:fr-turn2.xirsys.com:5349?transport=tcp"
-                            ]}
-
-                    ]
+                    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+                #     [
+                #     {
+                #         "urls": ["stun:fr-turn2.xirsys.com"]
+                #     }, {
+                #         "username": "PhUZzBq2wugeeSwzRxiwKCluHmp2OrZVfKvSWUMKVKJNK8B08EiCqVacNkLcfxkvAAAAAGTMeP1JcmluYQ==",
+                #         "credential": "1f5dbe1c-327c-11ee-a998-0242ac120004",
+                #         "urls": [
+                #             "turn:fr-turn2.xirsys.com:80?transport=udp",
+                #             "turn:fr-turn2.xirsys.com:3478?transport=udp",
+                #             "turn:fr-turn2.xirsys.com:80?transport=tcp",
+                #             "turn:fr-turn2.xirsys.com:3478?transport=tcp",
+                #             "turns:fr-turn2.xirsys.com:443?transport=tcp",
+                #             "turns:fr-turn2.xirsys.com:5349?transport=tcp"
+                #         ]}
+                #
+                # ]
                 },
 media_stream_constraints={"video": True, "audio": False},
 
