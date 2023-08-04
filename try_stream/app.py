08614426c3437e847b55da83bf5ff7c749b96903
IrDIE <iradynina00@mail.ru>
2023-08-04 19:26:57 +0300

    /

--- try_stream/app.py
+++ try_stream/app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
+import cv2
 import os
 import av
 import time
 import math
 
 
-import mediapipe as mp
 #import hand_tracking_module
 
 
